03a9e922f05c70da61f2e8238788632298191638
Khurshid <axurshidbek2005@gmail.com>
2022-11-03 17:31:36 +0500

    aos.js upgraded, bugs fixed, css file deleted

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function header() {
           <div className="container">
             <a className="navbar-brand" href="#!">
               <img src={logo} alt="" />
-              PHOTOFOX
+              PHOTOBOX
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
